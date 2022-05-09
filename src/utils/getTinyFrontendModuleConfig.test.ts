e4b91dda202ba4c7da375d99481aa29368fd2cdb
adrienbaron <adrien@abaron.net>
2022-05-09 11:03:16 +0100

    add basic tests for getTinyFrontendModuleConfig

---
+++ src/utils/getTinyFrontendModuleConfig.test.ts
@@ -0,0 +1,61 @@
+import { rest } from "msw";
+
+import { server } from "../mocks/server";
+import { TinyFrontendModuleConfig } from "../types";
+import { getTinyFrontendModuleConfig } from "./getTinyFrontendModuleConfig";
+
+describe("[getTinyFrontendModuleConfig]", () => {
+  it("should fetch the latest config and return it", async () => {
+    server.use(
+      rest.get(
+        "https://mock.hostname/api/tiny/latest/MOCK_LIB_NAME/MOCK_LIB_VERSION",
+        (_, res, ctx) =>
+          res(
+            ctx.status(200),
+            ctx.json({
+              umdBundle: "mockBundle.js",
+              cssBundle: "mockBundle.css",
+            } as TinyFrontendModuleConfig)
+          )
+      )
+    );
+
+    const tinyFrontendModuleConfig = await getTinyFrontendModuleConfig(
+      "MOCK_LIB_NAME",
+      "MOCK_LIB_VERSION",
+      "https://mock.hostname/api"
+    );
+
+    expect(tinyFrontendModuleConfig).toEqual({
+      umdBundle: "mockBundle.js",
+      cssBundle: "mockBundle.css",
+    });
+  });
+
+  it.each`
+    status
+    ${400}
+    ${401}
+    ${403}
+    ${500}
+  `("should throw an error on $status", async ({ status }) => {
+    server.use(
+      rest.get(
+        "https://mock.hostname/api/tiny/latest/MOCK_LIB_NAME/MOCK_LIB_VERSION",
+        (_, res, ctx) => res(ctx.status(status))
+      )
+    );
+
+    await expect(
+      getTinyFrontendModuleConfig(
+        "MOCK_LIB_NAME",
+        "MOCK_LIB_VERSION",
+        "https://mock.hostname/api"
+      )
+    ).rejects.toEqual(
+      new Error(
+        `Failed to fetch tiny frontend MOCK_LIB_NAME version MOCK_LIB_VERSION from API, with status ${status} and body ''`
+      )
+    );
+  });
+});
